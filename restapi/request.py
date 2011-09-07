b5073d3217c3e1d112cecf76d7ecf1ec5ca00dfc
Max Z. Chao <max.z.chao@gmail.com>
2011-09-07 20:58:30 +1000

    Fix issues of checking required params
    In most of the cases only one of required params is needed.

--- restapi/request.py
+++ restapi/request.py
@@ -29,7 +29,7 @@ class Request(object):
     def __init__(self, api_list):
         # help to pass 'api' into lambda
         def helper(api):
-            return lambda **args: self._make_request(api, **args)
+            return lambda **kargs: self._make_request(api, **kargs)
 
         self._apis = self._auto_dict()
         for a in api_list:
@@ -40,35 +40,41 @@ class Request(object):
             object.__setattr__(self, a[0], helper(a[0]))
 
     def _make_params(self, args, params, required, embeded):
-        for key in required:
-            if key and key not in args and key not in embeded:
-                raise RestAPIError('Missing required param:\'%s\'' % key)
-        
+        # check required params
+        # At least one of the requried params is needed
+        if required:
+            for key in required:
+                if key in args or key in embeded:
+                    break
+            else:
+                raise RestAPIError('Missing at least one required param:%s' % required)
+
+        # check the validation of input args
         for key in args:
             if key and key not in params:
                 raise RestAPIError('Unexcepted params:\'%s\'' % key)
-            
+
             if not params[key](args[key]):
                 raise RestAPIError('Params:\'%s\' is out of range' % key)
 
         return urllib.urlencode(args)
 
-    def _make_request(self, api, **args):
+    def _make_request(self, api, **kargs):
         url_format = self._apis[api]['url_format']
         method     = self._apis[api]['method']
         params     = self._apis[api]['params']
         required   = self._apis[api]['required']
 
         try:
-            url_prefix = url_format % args
+            url_prefix = url_format % kargs
         except KeyError:
-            raise RestAPIError('Missing required param:\'%s\'' % required)
+            raise RestAPIError('Missing at least one required param:%s' % required)
 
-        embeded    = re.findall('%\(([^\)]+)\)s', url_format)
+        embeded = re.findall('%\(([^\)]+)\)s', url_format)
         for p in embeded:
-            del args[p]
+            del kargs[p]
 
-        encoded    = self._make_params(args, params, required, embeded)
+        encoded = self._make_params(kargs, params, required, embeded)
 
         if encoded:
             url = url_prefix + '?' + encoded
