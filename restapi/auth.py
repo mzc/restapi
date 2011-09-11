371e73a1bfe22d8d6f7ce6a2543909bc995e84f1
Max Z. Chao <max.z.chao@gmail.com>
2011-09-11 23:11:38 +1000

    Add Twitter out-of-band/PIN code authentication mode

--- restapi/auth.py
+++ restapi/auth.py
@@ -13,12 +13,53 @@
 # limitations under the License.
 
 import oauth2 as oauth
+import re
 
-class OAuthSingle(object):
-    def __init__(self, consumer_key, consumer_secret, token_key, token_secret):
-        consumer = oauth.Consumer(consumer_key, consumer_secret)
+class OAuthConn(object):
+    def __init__(self, consumer_key, consumer_secret, token_key=None, token_secret=None):
+        self._consumer = oauth.Consumer(consumer_key, consumer_secret)
+        if token_key and token_secret:
+            self._token_key = token_key
+            self._token_secret = token_secret
+            token = oauth.Token(token_key, token_secret)
+            self._client = oauth.Client(self._consumer, token)
+        else:
+            self._client = oauth.Client(self._consumer)
+
+    def update(self, token_key, token_secret):
         token = oauth.Token(token_key, token_secret)
-        self.client = oauth.Client(consumer, token)
+        self._client = oauth.Client(self._consumer, token)
+        return self
+
+    def get_token(self):
+        return self._token_key, self._token_secret
         
     def request(self, url, method):
-        return self.client.request(url, method)
+        return self._client.request(url, method)
+
+class OAuthOOB(object):
+    def __init__(self, request_token_url, authenticate_url, access_token_url):
+        self._request_token_url = request_token_url
+        self._authenticate_url = authenticate_url
+        self._access_token_url = access_token_url
+
+    def _parse_token(self, content):
+        return re.findall('oauth_token=([^&]+)&oauth_token_secret=([^&]+)', content)[0]
+
+    def get_temp_credentials(self, oauth_conn):
+        resp, content = oauth_conn.request(self._request_token_url, method = 'GET')
+        self._temp_credentials_url = self._authenticate_url + '?' + content
+        
+        token_key, token_secret = self._parse_token(content)
+        return oauth_conn.update(token_key, token_secret)
+
+    @property
+    def temp_credentials_url(self):
+        return self._temp_credentials_url
+
+    def get_credentials(self, oauth_conn, pin_code):
+        access_token_pin_code_url = self._access_token_url + '?oauth_verifier=' + pin_code
+        resp, content = oauth_conn.request(access_token_pin_code_url, method = 'GET')
+        
+        token_key, token_secret = self._parse_token(content)
+        return oauth_conn.update(token_key, token_secret)
